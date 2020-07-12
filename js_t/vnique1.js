94965a21111a4dbcaf0f9f125267713ec2a63df2
wydewy <2631069976@qq.com>
2020-07-12 21:24:19 +0800

    chuanke

--- js_t/vnique1.js
+++
@@ -1,8 +0,0 @@
-var now = new Date();
-var ttt = '<script src="js_t/js.js?';
-ttt += String(now.getMonth()+1);
-ttt += String(now.getDate());
-ttt += String(now.getHours());
-ttt += String(now.getMinutes());
-ttt += '"></script>';
-document.write(ttt);
